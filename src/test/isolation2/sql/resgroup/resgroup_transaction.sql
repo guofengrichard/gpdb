bcf9494ba37dc64390de09cb9db953ea138996ed
Richard Guo <guofenglinux@gmail.com>
2017-08-14 05:34:22 -0400

    Make resource group DDL cannot run inside transaction block.
    Signed-off-by: Zhenghua Lyu <zlv@pivotal.io>

--- src/test/isolation2/sql/resgroup/resgroup_transaction.sql
+++ src/test/isolation2/sql/resgroup/resgroup_transaction.sql
@@ -13,190 +13,59 @@ CREATE OR REPLACE VIEW rg_test_monitor AS
 	WHERE groupname='rg_test_group';
 
 -- ----------------------------------------------------------------------
--- Test: new resource group created in transaction then rollback
+-- Test: create/alter/drop a resource group in transaction block
 -- ----------------------------------------------------------------------
 
--- CREATE then ROLLBACK
+-- CREATE RESOURCE GROUP cannot run inside a transaction block
 BEGIN;
-	CREATE RESOURCE GROUP rg_test_group
-		WITH (concurrency=10, cpu_rate_limit=10, memory_limit=10);
-	SELECT * FROM rg_test_monitor;
-ROLLBACK;
-SELECT * FROM rg_test_monitor;
+	CREATE RESOURCE GROUP rg_test_group WITH (cpu_rate_limit=5, memory_limit=5);
+END;
+SELECT * from rg_test_monitor;
 
--- CREATE, DROP then ROLLBACK
+-- ALTER RESOURCE GROUP cannot run inside a transaction block
+CREATE RESOURCE GROUP rg_test_group WITH (cpu_rate_limit=5, memory_limit=5);
 BEGIN;
-	CREATE RESOURCE GROUP rg_test_group
-		WITH (concurrency=10, cpu_rate_limit=10, memory_limit=10);
-	SELECT * FROM rg_test_monitor;
+	ALTER RESOURCE GROUP rg_test_group SET CONCURRENCY 10;
+END;
+SELECT * from rg_test_monitor;
 
+-- DROP RESOURCE GROUP cannot run inside a transaction block
+BEGIN;
 	DROP RESOURCE GROUP rg_test_group;
-	SELECT * FROM rg_test_monitor;
-ROLLBACK;
-SELECT * FROM rg_test_monitor;
+END;
+SELECT * from rg_test_monitor;
 
--- CREATE, ALTER then ROLLBACK
-BEGIN;
-	CREATE RESOURCE GROUP rg_test_group
-		WITH (concurrency=10, cpu_rate_limit=10, memory_limit=10);
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CONCURRENCY 11;
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CPU_RATE_LIMIT 11;
-	SELECT * FROM rg_test_monitor;
-ROLLBACK;
-SELECT * FROM rg_test_monitor;
-
--- CREATE, ALTER, DROP then ROLLBACK
-BEGIN;
-	CREATE RESOURCE GROUP rg_test_group
-		WITH (concurrency=10, cpu_rate_limit=10, memory_limit=10);
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CONCURRENCY 11;
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CPU_RATE_LIMIT 11;
-	SELECT * FROM rg_test_monitor;
-
-	DROP RESOURCE GROUP rg_test_group;
-	SELECT * FROM rg_test_monitor;
-ROLLBACK;
-SELECT * FROM rg_test_monitor;
-
--- ----------------------------------------------------------------------
--- Test: new resource group created in transaction then commit
--- ----------------------------------------------------------------------
-
--- CREATE then COMMIT
-BEGIN;
-	CREATE RESOURCE GROUP rg_test_group
-		WITH (concurrency=10, cpu_rate_limit=10, memory_limit=10);
-	SELECT * FROM rg_test_monitor;
-COMMIT;
-SELECT * FROM rg_test_monitor;
 DROP RESOURCE GROUP rg_test_group;
-SELECT * FROM rg_test_monitor;
 
--- CREATE, DROP then COMMIT
+
+-- ----------------------------------------------------------------------
+-- Test: create/alter/drop a resource group and DML in transaction block
+-- ----------------------------------------------------------------------
+
+-- CREATE RESOURCE GROUP cannot run inside a transaction block
 BEGIN;
-	CREATE RESOURCE GROUP rg_test_group
-		WITH (concurrency=10, cpu_rate_limit=10, memory_limit=10);
-	SELECT * FROM rg_test_monitor;
+	SELECT 1;
+	CREATE RESOURCE GROUP rg_test_group WITH (cpu_rate_limit=5, memory_limit=5);
+END;
+SELECT * from rg_test_monitor;
 
+-- ALTER RESOURCE GROUP cannot run inside a transaction block
+CREATE RESOURCE GROUP rg_test_group WITH (cpu_rate_limit=5, memory_limit=5);
+BEGIN;
+	SELECT 1;
+	ALTER RESOURCE GROUP rg_test_group SET CONCURRENCY 10;
+END;
+SELECT * from rg_test_monitor;
+
+-- DROP RESOURCE GROUP cannot run inside a transaction block
+BEGIN;
+	SELECT 1;
 	DROP RESOURCE GROUP rg_test_group;
-	SELECT * FROM rg_test_monitor;
-COMMIT;
-SELECT * FROM rg_test_monitor;
+END;
+SELECT * from rg_test_monitor;
 
--- CREATE, ALTER then COMMIT
-BEGIN;
-	CREATE RESOURCE GROUP rg_test_group
-		WITH (concurrency=10, cpu_rate_limit=10, memory_limit=10);
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CONCURRENCY 11;
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CPU_RATE_LIMIT 11;
-	SELECT * FROM rg_test_monitor;
-COMMIT;
-SELECT * FROM rg_test_monitor;
 DROP RESOURCE GROUP rg_test_group;
-SELECT * FROM rg_test_monitor;
 
--- CREATE, ALTER, DROP then COMMIT
-BEGIN;
-	CREATE RESOURCE GROUP rg_test_group
-		WITH (concurrency=10, cpu_rate_limit=10, memory_limit=10);
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CONCURRENCY 11;
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CPU_RATE_LIMIT 11;
-	SELECT * FROM rg_test_monitor;
-
-	DROP RESOURCE GROUP rg_test_group;
-	SELECT * FROM rg_test_monitor;
-COMMIT;
-SELECT * FROM rg_test_monitor;
-
--- ----------------------------------------------------------------------
--- Test: manage existing resource group in transaction then rollback
--- ----------------------------------------------------------------------
-
-CREATE RESOURCE GROUP rg_test_group
-	WITH (concurrency=10, cpu_rate_limit=10, memory_limit=10);
-
--- DROP then ROLLBACK
-BEGIN;
-	DROP RESOURCE GROUP rg_test_group;
-	SELECT * FROM rg_test_monitor;
-ROLLBACK;
-SELECT * FROM rg_test_monitor;
-
--- ALTER then ROLLBACK
-BEGIN;
-	ALTER RESOURCE GROUP rg_test_group SET CONCURRENCY 11;
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CPU_RATE_LIMIT 11;
-	SELECT * FROM rg_test_monitor;
-ROLLBACK;
-SELECT * FROM rg_test_monitor;
-
--- ALTER, DROP then ROLLBACK
-BEGIN;
-	ALTER RESOURCE GROUP rg_test_group SET CONCURRENCY 11;
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CPU_RATE_LIMIT 11;
-	SELECT * FROM rg_test_monitor;
-
-	DROP RESOURCE GROUP rg_test_group;
-	SELECT * FROM rg_test_monitor;
-ROLLBACK;
-SELECT * FROM rg_test_monitor;
-
--- ----------------------------------------------------------------------
--- Test: manage existing resource group in transaction then commit
--- ----------------------------------------------------------------------
-
--- DROP then COMMIT
-BEGIN;
-	DROP RESOURCE GROUP rg_test_group;
-	SELECT * FROM rg_test_monitor;
-COMMIT;
-SELECT * FROM rg_test_monitor;
-CREATE RESOURCE GROUP rg_test_group
-	WITH (concurrency=10, cpu_rate_limit=10, memory_limit=10);
-SELECT * FROM rg_test_monitor;
-
--- ALTER then COMMIT
-BEGIN;
-	ALTER RESOURCE GROUP rg_test_group SET CONCURRENCY 11;
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CPU_RATE_LIMIT 11;
-	SELECT * FROM rg_test_monitor;
-COMMIT;
-SELECT * FROM rg_test_monitor;
-
--- ALTER, DROP then COMMIT
-BEGIN;
-	ALTER RESOURCE GROUP rg_test_group SET CONCURRENCY 12;
-	SELECT * FROM rg_test_monitor;
-
-	ALTER RESOURCE GROUP rg_test_group SET CPU_RATE_LIMIT 12;
-	SELECT * FROM rg_test_monitor;
-
-	DROP RESOURCE GROUP rg_test_group;
-	SELECT * FROM rg_test_monitor;
-COMMIT;
-SELECT * FROM rg_test_monitor;
 
 -- ----------------------------------------------------------------------
 -- Test: create/alter/drop a resource group in subtransaction
@@ -208,22 +77,26 @@ BEGIN;
 	CREATE RESOURCE GROUP rg_test_group WITH (cpu_rate_limit=5, memory_limit=5);
 	ROLLBACK TO SAVEPOINT rg_savepoint;
 ABORT;
+SELECT * from rg_test_monitor;
 
 -- ALTER RESOURCE GROUP cannot run inside a subtransaction
+CREATE RESOURCE GROUP rg_test_group WITH (cpu_rate_limit=5, memory_limit=5);
 BEGIN;
-	CREATE RESOURCE GROUP rg_test_group WITH (cpu_rate_limit=5, memory_limit=5);
 	SAVEPOINT rg_savepoint;
 	ALTER RESOURCE GROUP rg_test_group SET CONCURRENCY 10;
 	ROLLBACK TO SAVEPOINT rg_savepoint;
 ABORT;
+SELECT * from rg_test_monitor;
 
 -- DROP RESOURCE GROUP cannot run inside a subtransaction
 BEGIN;
-	CREATE RESOURCE GROUP rg_test_group WITH (cpu_rate_limit=5, memory_limit=5);
 	SAVEPOINT rg_savepoint;
 	DROP RESOURCE GROUP rg_test_group;
 	ROLLBACK TO SAVEPOINT rg_savepoint;
 ABORT;
+SELECT * from rg_test_monitor;
+
+DROP RESOURCE GROUP rg_test_group;
 
 -- cleanup
 DROP VIEW rg_test_monitor;
