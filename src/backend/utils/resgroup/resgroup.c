f7da4bf79f396e3002944ad5f6a5b1bd8e6aae60
Richard Guo <guofenglinux@gmail.com>
2018-02-23 05:35:16 +0000

    Maintain cgroup memory hierarchy for resource group.

--- src/backend/utils/resgroup/resgroup.c
+++ src/backend/utils/resgroup/resgroup.c
@@ -491,6 +491,7 @@ InitResGroups(void)
 		ResGroupData	*group;
 		int cpuRateLimit;
 		Oid groupId = HeapTupleGetOid(tuple);
+		int fd;
 
 		GetResGroupCapabilities(relResGroupCapability, groupId, &caps);
 		cpuRateLimit = caps.cpuRateLimit;
@@ -500,6 +501,10 @@ InitResGroups(void)
 
 		ResGroupOps_CreateGroup(groupId);
 		ResGroupOps_SetCpuRateLimit(groupId, cpuRateLimit);
+
+		fd = ResGroupOps_LockGroup(groupId, "memory", true);
+		ResGroupOps_SetMemoryLimitByRate(groupId, caps.memLimit * ResGroup_GetSegmentNum());
+		ResGroupOps_UnLockGroup(groupId, fd);
 
 		numGroups++;
 		Assert(numGroups <= MaxResourceGroups);
@@ -782,6 +787,12 @@ ResGroupGetExtension(char *name)
 	}
 
 	return -1;
+}
+
+int
+ResGroup_GetSegmentNum()
+{
+	return (Gp_role == GP_ROLE_EXECUTE ? host_segments : pResGroupControl->segmentsOnMaster);
 }
 
 static char *
